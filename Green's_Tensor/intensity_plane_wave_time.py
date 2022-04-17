03ceaf91291ea9d2f995c771acf3d6ac6620f071
Romek Kilianski <r.kilianski.1@research.gla.ac.uk>
2022-04-17 10:52:47 +0100

    plane wave in 2D modified

--- Green's_Tensor/intensity_plane_wave_time.py
+++ Green's_Tensor/intensity_plane_wave_time.py
@@ -3,12 +3,12 @@ import meep as mp
 import numpy as np
 from matplotlib import pyplot as plt
 
-DPML = 1  # thickness of perfectly matched layers (PMLs) around the box
+DPML = 2  # thickness of perfectly matched layers (PMLs) around the box
 PML_LAYERS = [mp.PML(DPML)]
 DT = 5
 T = 100
-FCEN = 5 / np.pi
-ITERATIONS = 200
+FCEN = 2 / np.pi
+ITERATIONS = 10
 
 OBS_X_A, OBS_Y_A = 10, 10  # dimensions of the computational cell, not including PML
 OBS_VOL = mp.Vector3(OBS_X_A, OBS_Y_A)
@@ -16,14 +16,14 @@ CELL_X, CELL_Y = 20, 20
 CELL = mp.Vector3(CELL_X + 2 * DPML, CELL_Y + 2 * DPML)
 
 SOURCE_POSITION_X, SOURCE_POSITION_Y = -OBS_X_A / 2, 0
-OBS_POSITION_X, OBS_POSITION_Y = 0, -OBS_Y_A / 2
+OBS_POSITION_X, OBS_POSITION_Y = 0, 0
 
 MATERIAL = mp.Medium(epsilon=1)
 OMEGA = np.pi  # angular frequency of emitter
 
 RESOLUTION = 8
 PIXEL_SIZE = 1 / RESOLUTION
-BLOCK_SIZE = 2 * PIXEL_SIZE
+BLOCK_SIZE = 3 * PIXEL_SIZE
 GEOMETRY = []
 x_points = []
 y_points = []
@@ -48,7 +48,8 @@ def df(old_field_arr, adj_field_arr):
     e1, e2, e3, eps1 = old_field_arr
     a1, a2, a3, eps2 = adj_field_arr
     merit_function = np.real(a1 * e1 + a2 * e2)
-    return merit_function
+    norm = 1 / (np.amax(merit_function))
+    return norm * merit_function
 
 
 # Produce a current source from one of the sides o the box
@@ -78,7 +79,7 @@ def pw_maker(freq, kDir, pol, srcPos, srcBox, complex_phase=1):
 
 k1 = [1, 0]
 
-sourceLineX = mp.Vector3(0, CELL_Y / 8)
+sourceLineX = mp.Vector3(0, CELL_Y)
 
 # Plane wave source for input wave
 
@@ -117,7 +118,6 @@ def produce_adjoint_field(forward_field):
 
 
 def get_fields(simulation):
-
     fields_data = [simulation.get_array(center=mp.Vector3(), size=OBS_VOL, component=field) for field in components]
     fields_data_elements = [a[1:-1, 1:-1] for a in fields_data]
     # ex,ey,ez,epsilon
@@ -148,7 +148,9 @@ sim_adjoint = mp.Simulation(
 )
 
 sim_adjoint.run(until=T)
+
 adjoint_field = get_fields(sim_adjoint)
+
 delta_f = df(old_field, adjoint_field)
 
 ########################################################################################################################
@@ -247,6 +249,7 @@ for i in range(ITERATIONS):
     #  Calculating the merit function df
     delta_f = df(old_field, adjoint_field)
     delta_f[x_obs_index:, :] = np.zeros((len(x) - x_obs_index, len(y)))
+    delta_f[:5, :] = np.zeros((5, len(y)))
     #  picking the coordinates corresponding to the highest change in df
     x1, y1 = pick_max(delta_f)
     #  updating the geometry
@@ -256,15 +259,19 @@ for i in range(ITERATIONS):
     x_points.append(x1)
     y_points.append(y1)
 
-    intensity_ratio = np.array(intensity_at_obs)/np.array(intensity_at_source[0])
-    intensity_ratio_obs_obs = np.array(intensity_at_obs)/np.array(intensity_at_obs[0])
+    intensity_ratio = np.array(intensity_at_obs) / np.array(intensity_at_source[0])
+    intensity_ratio_obs_obs = np.array(intensity_at_obs) / np.array(intensity_at_obs[0])
 
 Ex, Ey, Ez, eps_data = old_field
 Ex_a, Ey_a, Ez_a, eps_data_a = adjoint_field
 eps_data = np.ma.masked_array(eps_data, eps_data < np.sqrt(1.4))
 
 e_squared_a = np.real((Ex_a * np.conjugate(Ex_a) + Ey_a * np.conjugate(Ey_a) + Ez_a * np.conjugate(Ez_a)))
+e_squared_a = (1 / np.amax(e_squared_a)) * e_squared_a
+
 e_squared = np.real((Ex * np.conjugate(Ex) + Ey * np.conjugate(Ey) + Ez * np.conjugate(Ez)))
+e_squared = (1 / np.amax(e_squared)) * e_squared
+
 ########################################################################################################################
 #                                                   PLOTTING
 ########################################################################################################################
@@ -280,23 +287,23 @@ ax[0, 0].set_title('Ex')
 ax[0, 0].pcolormesh(x, y, np.transpose(np.real(eps_data)), cmap='Greys', alpha=1, vmin=0, vmax=4)
 ax[0, 0].plot(x[x_obs_index], y[y_obs_index], 'ro')
 
-# ax[0, 1].pcolormesh(x, y, np.transpose(np.real(Ey)))
-# ax[0, 1].set_title('Ey')
-# ax[0, 1].pcolormesh(x, y, np.transpose(np.real(eps_data)), cmap='Greys', alpha=1, vmin=0, vmax=4)
-# ax[0, 1].plot(x[x_obs_index], y[y_obs_index], 'ro')
+ax[0, 1].pcolormesh(x, y, np.transpose(np.real(Ey)))
+ax[0, 1].set_title('Ey')
+ax[0, 1].pcolormesh(x, y, np.transpose(np.real(eps_data)), cmap='Greys', alpha=1, vmin=0, vmax=4)
+ax[0, 1].plot(x[x_obs_index], y[y_obs_index], 'ro')
 
-ax[0,1].plot(blocks_added, intensity_ratio_obs_obs)
-ax[0, 1].set_title('(I at obs)/(I at obs at t=0) after adding a block')
+# ax[0,1].plot(blocks_added, intensity_ratio_obs_obs)
+# ax[0, 1].set_title('(I at obs)/(I at obs at t=0) after adding a block')
 
-ax[0, 2].plot(blocks_added, intensity_ratio)
-ax[0, 2].set_title('(I at obs)/(I at source at t=0) after adding a block')
+ax[0, 2].plot(blocks_added, intensity_at_obs)
+ax[0, 2].set_title('Intensity after adding a block')
 
 i = 0
 
 for ax, Si, name in zip([ax[1, 0], ax[1, 1], ax[1, 2]],
                         [e_squared, e_squared_a, delta_f],
                         ['E field Intensity', 'Adjoint field intensity', "Merit Function"]):
-    S_ax = ax.pcolormesh(x, y, np.transpose(Si), vmax=1e-3, vmin=0, cmap='RdYlBu')
+    S_ax = ax.pcolormesh(x, y, np.transpose(Si), cmap='RdYlBu')
     ax.pcolormesh(x, y, np.transpose(np.real(eps_data)), cmap='Greys', alpha=1, vmin=0, vmax=4)
     ax.set_title(name)
     cbar_ax = fig.add_axes([0.85, 0.1, 0.02, 0.5])
